eb3ff800ca9825b6fff8b4bfa508cfa01d5f0b15
“Marcus” <115784464+Marcussy34@users.noreply.github.com>
2026-01-27 14:26:28 +0800

    add Google Fonts for Umbra theme

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,10 @@ export default function Document() {
           src="https://unpkg.com/snarkjs@latest/build/snarkjs.min.js"
           defer
         />
+        {/* Google Fonts for the Umbra theme */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@400;500;600;700&family=Oswald:wght@400;500;600;700&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet" />
       </Head>
       <body className="antialiased">
         <Main />
